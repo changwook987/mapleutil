7b666f3d16b8573ca4755d3d762819b2d035dab6
cho changwook <changwook987@abc>
2023-04-05 10:33:29 +0900

    :wastebasket: cleanup project structure & :heavy_plus_sign: added Khali

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from "./Main";
-import NotFound from "./NotFound";
-import Header from "./Header";
-import Footer from "./Footer";
-import Randomchar from "./randomchar";
-import Randombob from "./randombob";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main/Main";
+import NotFound from "./components/NotFound/NotFound";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Randomchar from "./components/RandomChar/RandomChar";
+import Randombob from "./components/Randombob/randombob";
 
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 
 const App = () => {
-  return(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Header />
-      <div className={styles.Layout}>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/randomchar" element={<Randomchar />}></Route>
-          <Route path="/randombob" element={<Randombob />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </div>
-      <Footer className={styles.bottom}/>
-    </BrowserRouter>
-  )
-}
+    return (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Header />
+            <div className={styles.Layout}>
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="/randomchar" element={<Randomchar />}></Route>
+                    <Route path="/randombob" element={<Randombob />}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </div>
+            <Footer className={styles.bottom} />
+        </BrowserRouter>
+    );
+};
 export default App;
